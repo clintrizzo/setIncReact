faa2538df222432b6e752792e10892822f3aa51a
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-08-17 14:42:33 -0700

    Updated navbar and font awesome icons for current navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './components/navbar/navbar.css'
 import Navbar from './components/navbar/navbar'
 
 class App extends Component {
