68d94cfb8f233e3f9015229db64a11b20a58498f
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-08-19 20:49:21 -0700

    Demo landing page. Need to update text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar/navbar'
+import About from './components/landingpage/aboutus.js'
 import Footer from './components/footer/footer'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
+        <About />
         <Footer />
       </div>
     );
