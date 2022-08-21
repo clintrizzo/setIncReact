3d84dde087f4f699ae7a2a5ac650dac8a03e2edf
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-08-20 17:21:23 -0700

    test

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,21 +10,20 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className='navbar' sticky='top'>
       <Container>
-        <Navbar.Brand style={{color: 'white'}}>
+        <Navbar.Brand style={{color: 'black'}}>
         <img
         className='rotateimg180'
         src="../images/setloco.ico"
         width="35"
         height="35"
-        color='white'
         />{' Strategic Electrical Technologies'}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav" style={{marginLeft: '60px'}}>
+        <Navbar.Collapse id="basic-navbar-nav" className='navHeader'>
           <Nav className="me-auto">
             <Link to="/" style={{color: 'white'}} id='pages'>Home</Link>
             <Link to="/merchandise" style={{color: 'white'}} id='pages'>Merchandise</Link>
-            <NavDropdown title="Social Links" id="basic-nav-dropdown" className='socialIcons'>
+            <NavDropdown title="Social Links" id="basic-nav-dropdown" className='socialIcons' style={{fontFamily: "Georgia, 'Times New Roman', Times, serif;"}}>
               <NavDropdown.Item href="https://www.instagram.com/setincusa/" rel="noopener noreferrer" target="_blank">
                 <i className="fa fa-instagram" aria-hidden="true" style={{paddingRight: '5px', color: 'black'}}/> Instagram
               </NavDropdown.Item>
