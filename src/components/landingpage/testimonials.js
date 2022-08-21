b75c25b3e146f97b6262037b40a0d640191544bd
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-08-21 12:16:52 -0700

    start of testimonial cards

--- src/components/landingpage/testimonials.js
+++ src/components/landingpage/testimonials.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-
+import Card from '../cards/testimonialCards.js'
 class Testimonials extends Component {
   render() {
     return(
       <div className='testimonials'>
         <hr style={{borderTop: '3px solid red',  width: '100%'}}/>
         <center>
-          <h4 className='customerWords'>Testimonials</h4>
+          <h3 className='customerWords'>Testimonials</h3>
           <p>A few words from our previous and current clients.</p>
         </center>
+        <Card />
       </div>
     )
   }
